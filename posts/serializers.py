de514f1b3b548261699393db3af82e3d671eb252
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-03-08 17:21:10 +0200

    add viewsets, routers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        many=False
+    )
+
     class Meta:
         model = Post
         fields = (
@@ -11,4 +18,12 @@ class PostSerializer(serializers.ModelSerializer):
             'title',
             'body',
             'created_at',
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'id', 'username',
         )
